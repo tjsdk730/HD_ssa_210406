2cfe9dc1bcad6a90e36dfec279c7b72741cd6ea5
DESKTOP-RDD88MA\LG <tjsdk2486@gmil.com>
2021-04-06 21:50:50 +0900

    예제1

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main{
+	int a = 10, b = 30, c = 200, d = 500;
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    printf("%d\n", d);
+    fgetc(stdin);
+
+
+}
